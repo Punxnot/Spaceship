7a53cc71973e78cffc3eac619452bf722fd88730
unknown <ozma_1@mail.ru>
2015-03-29 01:15:00 +0300

    Random rendering

--- Assets/scripts/AsteroidsController.cs
+++ Assets/scripts/AsteroidsController.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class AsteroidsController : MonoBehaviour {
 
 	public GameObject bullet;
+	public float tumble;
 
 	// Use this for initialization
 	void Start () {
-	
+		GetComponent<Rigidbody2D>().angularVelocity = Random.value * tumble;
+
+		GetComponent<Rigidbody2D>().position = new Vector2(Random.Range(-5.0F, 5.0F), Random.Range(-5.0F, 5.0F));
 	}
 
 	void OnCollisionEnter2D(Collision2D bullet) {
@@ -17,6 +20,6 @@ public class AsteroidsController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
